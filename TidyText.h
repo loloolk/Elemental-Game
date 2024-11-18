d4b2003cd5b879c2fd8d50b50db2fef6cefe7b08
loloolk <marc.bondoger@gmail.com>
2024-11-18 11:30:08 -0800

    Renamed badly named header file

--- TidyText.h
+++ TidyText.h
@@ -1,32 +1,5 @@
+#pragma once
 #include <string>
-
-#ifndef factorial_
-#define factorial_
-
-int factorial(int number) {
-    int iteration, factorial = 1;
-    for(iteration = 1; iteration <= number; iteration++) {
-        factorial=factorial*iteration;
-    }
-    return factorial;
-}
-
-int powerOf(int num1, int num2) {
-    int b = num1;
-    for (int i = 1; i <= num2; i++) {
-        num1 *= b;
-    }
-    return num1;
-}
-
-bool noSpace(std::string a) {
-    for (int i = 0; i < a.length(); i++) {
-        if (a[i] == ' ') {
-            return true;
-        }
-    }
-    return false;
-}
 
 std::string tidyText(std::string a) {
     a[0] = toupper(a[0]);    
@@ -52,10 +25,3 @@ std::string tidyText(std::string a) {
     }
     return a;
 }
-
-/*int randNum(int i) {
-    srand((unsigned) time(0));
-    return (rand() % i) + 1;
-}*/
-
-#endif
